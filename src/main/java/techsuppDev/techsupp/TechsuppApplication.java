5d823c0644c53e0dea01e3e137534567ea3684b7
thumbthing <ilscc0902@gmail.com>
2023-02-07 11:43:19 +0900

    라이브러리 설정
    lombok
    Thymeleaf
    Spring Web
    WebSocket
    MySql Driver(db 연결 전이어서 주석처리)
    Spring Security

---
+++ src/main/java/techsuppDev/techsupp/TechsuppApplication.java
@@ -0,0 +1,15 @@
+package techsuppDev.techsupp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsuppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsuppApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/techsuppDev/techsupp/TechsuppApplicationTests.java
@@ -0,0 +1,13 @@
+package techsuppDev.techsupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsuppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
